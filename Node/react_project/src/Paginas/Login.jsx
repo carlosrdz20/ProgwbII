77dd73e114c28c606a830f12db45e4edad0ec39c
carlosrdz20 <carlos.arteagardrg@uanl.edu.mx>
2024-02-28 15:29:10 -0600

    Validaciones
    Validaciones de las páginas de Registro y Login.

--- Node/react_project/src/Paginas/Login.jsx
+++ Node/react_project/src/Paginas/Login.jsx
@@ -1,13 +1,38 @@
 import { Link, useNavigate } from "react-router-dom";
 import "../Estilos/Login.css";
 import Logo from "../Componentes/Logo.jsx";
+import React, { useState } from 'react';
 
 export default function Login() {
   const navigate = useNavigate();
 
+  const [formData, setFormData] = useState({
+     email: '',
+      password: '' });
+
   const Enviar = (evento) => {
     evento.preventDefault();
+    
+    // Validación del correo electrónico
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(formData.email)) {
+      alert("Por favor ingresa un correo electrónico válido");
+      return;
+    }
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+}{"':;?/>.<,])(?=.*[a-zA-Z]).{6,}$/;
+    // Validación de la contraseña (por ejemplo, longitud mínima)
+    if (formData.password.length < 6 || formData.password.includes(" ")) {
+      alert("La contraseña debe contener al menos una mayúscula, una minúscula, un dígito, un carácter especial y ser mayor a 6 caracteres");
+      return;
+    }
+
+    // Si todas las validaciones son exitosas, puedes enviar el formulario
     navigate('/');
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
   return (
@@ -17,10 +42,10 @@ export default function Login() {
         <Logo Imagen={'favicon.ico'}/>
         <form action="#" onSubmit={Enviar}>
           <div className="textbox">
-            <input type="email" placeholder="Correo electrónico" name="email" required />
+            <input type="email" placeholder="Correo electrónico" name="email" value={formData.email} onChange={handleChange} required />
           </div>
           <div class="textbox">
-            <input type="password" placeholder="Contraseña" name="password" required />
+            <input type="password" placeholder="Contraseña" name="password" value={formData.password} onChange={handleChange} required />
           </div>
           <button type="submit" className="btn-IniSes">Iniciar Sesión <strong>{'>'}</strong> </button>
         </form>
