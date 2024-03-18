5e529aa36c80b776f71e0aff8d74805436054fcc
P4NCH1T0 <98226655+P4NCH1T0@users.noreply.github.com>
2024-03-17 21:05:09 -0600

    Crear publicacion

---
+++ Node/react_project/src/Componentes/CrearPublicacionCuerpo.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import '../Estilos/CrearPublicacion.css';
+import { MenuLateral } from "./MenuIzquierdo.jsx";
+
+function CrearPublicacion() {
+
+    return (
+    <div className="Cuerpo">
+        <div>
+          <MenuLateral />
+        </div>
+        <div className="RecuadoPublicacion">
+          
+        </div>
+    </div>   
+    );
+  }
+  
+  export default CrearPublicacion;
+  
