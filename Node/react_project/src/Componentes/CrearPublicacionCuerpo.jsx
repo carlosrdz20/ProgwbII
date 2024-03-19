4e5f1f32df706a045b872e322f4e00be8927350f
P4NCH1T0 <98226655+P4NCH1T0@users.noreply.github.com>
2024-03-18 23:06:55 -0600

    Pagina de crear publicación
    Falta ser checada por leo

--- Node/react_project/src/Componentes/CrearPublicacionCuerpo.jsx
+++ Node/react_project/src/Componentes/CrearPublicacionCuerpo.jsx
@@ -9,9 +9,45 @@ function CrearPublicacion() {
         <div>
           <MenuLateral />
         </div>
-        <div className="RecuadoPublicacion">
-          
+        <div class="container">
+        <div class="box">
+            <div class="column">
+                <div class="input-container">
+                    <label for="input1" class="input-label">Título:</label>
+                    <input type="text" id="input1" class="blue-border" placeholder="Ingrese aquí"/>
+                </div>
+                <div class="input-container">
+                    <label for="input2" class="input-label">Descripción:</label>
+                    <input type="text" id="input2" class="blue-border" placeholder="Ingrese aquí"/>
+                </div>
+            </div>
+            <div class="column">
+                <div class="input-container">
+                    <label for="combobox" class="input-label">País:</label>
+                    <select id="combobox" class="blue-border">
+                        <option value="opcion1">Opción 1</option>
+                        <option value="opcion2">Opción 2</option>
+                        <option value="opcion3">Opción 3</option>
+                    </select>
+                </div>
+                <div class="button-container">
+                    <button class="button">Guardar como borrador</button>
+                </div>
+            </div>
+            <div class="column">
+                <div class="input-container">
+                    <label for="photoInput" class="input-label">Publicar:</label>
+                    <input type="file" id="photoInput" accept="image/*" class="blue-border"/>
+                </div>
+                <div class="image-container">
+                    <img src="" alt="Imagen" id="uploadedImage"/>
+                </div>
+                <div class="button-container">
+                    <button class="button">Enviar</button>
+                </div>
+            </div>
         </div>
+    </div>
     </div>   
     );
   }
