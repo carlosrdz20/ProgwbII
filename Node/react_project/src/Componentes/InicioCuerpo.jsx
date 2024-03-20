fdbdf964cc29cbfaae3b6efa99d428f7227d662a
KFecito233 <leo_cervant@outlook.com>
2024-03-19 18:56:30 -0600

    Nuevo Prop en PublicDisplay
    Se ha agregado un prop llamado "Tipo" que se declara para poder saber que tipo de publicación es, sí es Propio, Ajeno o un Borrador, y cambian algunos botones o funciones en las en las publicaciones

--- Node/react_project/src/Componentes/InicioCuerpo.jsx
+++ Node/react_project/src/Componentes/InicioCuerpo.jsx
@@ -53,6 +53,7 @@ function InicioCuerpo() {
                 Pais={'Bandera.png'}
                 Contenido={'Lorem Ipsum Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor.'}
                 Imagen={'Registro_BG.jpg'}
+                Tipo={'Propio'}
               />
             </Col>
           </Row>
--- Node/react_project/src/Componentes/PublicacionDisplay.jsx
+++ Node/react_project/src/Componentes/PublicacionDisplay.jsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import '../Estilos/PublicacionDisplay.css'
 import { Row, Col } from 'react-bootstrap';
 import Rating from "./Rating";
+import { FaPencil } from "react-icons/fa6";
+import { FaRegPaperPlane, FaTrash } from "react-icons/fa";
+
 
 function Corazon(props) {
 	const [active, setActive] = useState(false)
@@ -14,7 +17,7 @@ function Corazon(props) {
 	);
 }
 
-function PublicDisplay({ NombreUsu, ImagenUsu, Fecha, Pais, Contenido, Imagen }){
+function PublicDisplay({ NombreUsu, ImagenUsu, Fecha, Pais, Contenido, Imagen, Tipo }){
 	const [rating, setRating] = useState(0);
 
   const handleRatingChange = (newRating) => {
@@ -25,23 +28,43 @@ function PublicDisplay({ NombreUsu, ImagenUsu, Fecha, Pais, Contenido, Imagen })
 		<div className="PublicContenedor">
 			<Row>
 				<Col className="UsuInfo" md={12}>
-					<Col lg={3}>
+					<Col className="ColImg" lg={3}>
 						<img className="ImgUsu" src={`/Imagenes/${ImagenUsu}`} alt="Imagen de Usuario" />
 					</Col>
 					<Col lg={7} className="DivCentro">
-						<div className="NomBot">
-							<Link className="LinkUsuName">{NombreUsu}</Link>
-							<button>+Seguir</button>							
-						</div>
+						{Tipo === 'Propio' ? (
+							<div className="NomBot">
+								<Link className="LinkUsuName" to={'/Perfil'}>{NombreUsu}</Link>				
+							</div>
+						) : Tipo === 'Ajeno' ? (
+							<div className="NomBot">
+								<Link className="LinkUsuName">{NombreUsu}</Link>
+								<button>+Seguir</button>
+							</div>
+						) : Tipo === 'Borrador' ? (
+							<div className="NomBot">
+								<Link className="LinkUsuName" to={'/Perfil'}>{NombreUsu}</Link>
+							</div>
+						) : null}
 						<div className="FechaRating">
 							<p>{Fecha}</p>
 							<Rating initialRating={rating} onRatingChange={handleRatingChange} />
 						</div>
 					</Col>
-					<Col lg={2} className="PaisReacc">
-						<img className="PublicBan" src={`/Imagenes/${Pais}`} alt="Bandera" />
-						<Corazon/>
-					</Col>
+						{Tipo === 'Propio' ? (
+							<Col lg={2} className="PaisReacc">
+								<img className="PublicBan" src={`/Imagenes/${Pais}`} alt="Bandera" />
+							</Col>
+						) : Tipo === 'Ajeno' ? (
+							<Col lg={2} className="PaisReacc">
+								<img className="PublicBan" src={`/Imagenes/${Pais}`} alt="Bandera" />
+								<Corazon />
+							</Col>
+						) : Tipo === 'Borrador' ? (
+							<Col lg={2} className="PaisReacc">
+								<img className="PublicBan" src={`/Imagenes/${Pais}`} alt="Bandera" />
+							</Col>
+						) : null}
 				</Col>
 				<Col className="PublicContenido" md={12}>
 					<div>
@@ -50,6 +73,20 @@ function PublicDisplay({ NombreUsu, ImagenUsu, Fecha, Pais, Contenido, Imagen })
 					<div className="divImagen">
 						<img className="PublicImagen" src={`/Imagenes/${Imagen}`} alt="Imagen de Contenido" />
 					</div>
+					{Tipo === 'Propio' ? (
+							<div className="DivButtons">
+								<button><FaTrash /></button>
+								<button><FaPencil /></button>
+							</div>
+						) : Tipo === 'Ajeno' ? (
+							<></>
+						) : Tipo === 'Borrador' ? (
+							<div className="DivButtons">
+								<button><FaTrash /></button>
+								<button><FaPencil /></button>
+								<button><FaRegPaperPlane /></button>
+							</div>
+						) : null}
 				</Col>
 			</Row>
 		</div>
